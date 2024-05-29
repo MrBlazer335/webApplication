e05ae6b92957c306b797bbf245a55718fe3a7df8
Leomir <leomir2002@gmail.com>
2024-05-29 20:07:34 +0300

    Heroku fix

--- src/main/java/heroku/leomir/main/webApp/WebAppApplication.java
+++ src/main/java/heroku/leomir/main/webApp/WebAppApplication.java
@@ -2,8 +2,10 @@ package heroku.leomir.main.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@ResponseBody
 public class WebAppApplication {
 
 	public static void main(String[] args) {
